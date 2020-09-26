d8c1a7040113aeea8639b47a15be6991390f6521
walter-chingg05 <u1803490@unimilitar.edu.co>
2020-09-26 10:38:10 -0500

    inicio de proyecto en codigo

---
+++ Laboratorio3/src/laboratorio3/Laboratorio3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio3;
+
+/**
+ *
+ * @author walle
+ */
+public class Laboratorio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //todo codigo obtenido se experimentara en este java para no tener que estar enredandonos con distintos archivos al final.
+    }
+    
+}
